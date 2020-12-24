5b7d8ced9c2b2b0c1c693519431b952aa0ccb0d7
totherain155 <totherain155@gmail.com>
2020-12-24 14:55:25 +0900

    practice

---
+++ practice11.js
@@ -0,0 +1,9 @@
+let sum = 0;
+
+for (i = 1; i <= 100; i++) {
+    //1+2+3+4+5+...100
+    sum = sum + i
+    // console.log(sum)
+}
+
+console.log(sum)
---
+++ practice9.js
@@ -0,0 +1,15 @@
+var year = '2019';
+var month = '04';
+var day = '26';
+var hour = '11';
+var minute = '34';
+var second = '27';
+
+var result = year.concat('/', month, '/', day, '/', '', hour, ':', minute, ':', second)
+
+console.log(result);
+
+// concatenate 연결하다라는 의미
+
+
+//출력 2019 / 04 / 26 11: 34: 27
