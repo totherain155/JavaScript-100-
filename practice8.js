d45f1c6dc9dbf7a06e7188c4838c2f60b73b1cac
totherain155 <totherain155@gmail.com>
2020-12-24 09:07:14 +0900

    practice8

---
+++ practice8.js
@@ -0,0 +1,13 @@
+const d = {
+    'height': 180,
+    'weight': 78,
+    'weight': 84,
+    'temperature': 36,
+    'eyesight': 1
+};
+
+console.log(d.weight);
+
+
+/* 객체에 접근하는 법은 .과 [] 방법이 있는데
+   []로 접근하려면 ''을 붙여줘야 한다.  */
