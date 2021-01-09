a03edc67d588027f222446567c474d0844c61621
totherain155 <totherain155@gmail.com>
2021-01-09 12:39:53 +0900

    practice

---
+++ practice23.js
@@ -0,0 +1,4 @@
+// console.log(10/3)의 출력 결과과 3이 되게 하려면 ... 
+
+
+console.log(Math.floor(10 / 3)) 
---
+++ practice24.js
@@ -0,0 +1,7 @@
+const person = "kim"
+
+console.log(person.toUpperCase())
+
+const person1 = "KIM"
+
+console.log(person.toLowerCase())
