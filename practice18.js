5fa4dd7e4eefdca9f4e565a5c1990e2387e8fe99
totherain155 <totherain155@gmail.com>
2021-01-09 10:22:23 +0900

    practice18

--- practice18.js
+++ practice18.js
@@ -1,12 +1,15 @@
-const score = prompt().split(' ');
+//  세개의 입력점수를 받아 평균 구하기 
 
-let sum = 0;
+const subject = prompt().split(' ')
+let sum = 0
 
-for (let i = 0; i < score.length; i++) {
-    sum = sum + parseInt(score[i], 10);
+for (let i = 0; i < subject.length; i++) {
+    sum = sum + parseInt(subject[i], 10)
 }
 
-console.log(Math.floor(sum / score.length));
+console.log(Math.floor(sum / subject.length))
+
+
 
 //Math floor 주어진 값보다 이하의 가장 큰 정수를 반환합니다.
 // 예를들어 -70.333 인경우 -71이 반환된다.
