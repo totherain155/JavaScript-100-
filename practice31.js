45d8cc7cc7fa73fbbcdf7f5983999ab5d448f286
totherain155 <totherain155@gmail.com>
2021-02-20 18:00:33 +0900

    practice31

---
+++ practice31.js
@@ -0,0 +1,13 @@
+/*
+1)  arr[i]
+2)  arr.push(5)
+3)  arr.slice()
+4)  arr.pop()
+5)  arr.includes(5)
+
+*/
+
+// 시간 복잡도가 O(1)이 아닌 것
+
+// arr.slice() 원하는 만큼 잘라서 array로 반환
+// arr.includes() 해당 item을 검색한 후 boolean으로 반환 
