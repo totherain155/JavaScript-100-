5af61d70771982f1bab60659709f95e47e3d9703
totherain155 <totherain155@gmail.com>
2021-02-22 23:35:32 +0900

    practice

---
+++ practice33.js
@@ -0,0 +1,9 @@
+const nums = "1 2 3 4 5";
+const arr = prompt().split(" ").reverse();
+let reverseVal = "";
+
+for (let i = 0; i < arr.length; i++) {
+    reverseVal = reverseVal + (arr[i] + " ");
+}
+
+console.log(reverseVal)
