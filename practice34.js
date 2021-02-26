e6acb2c2d8b03d297b41268953f4019f0f596682
totherain155 <totherain155@gmail.com>
2021-02-26 23:25:17 +0900

    practice

--- practice34.js
+++ practice34.js
@@ -1,16 +1,7 @@
-const unsorted = prompt();
-let sorted = "";
-
-sorted = unsorted.split(" ").sort(function (a, b) {
-    return a - b;
+const unsorted = prompt(" ")
+const sorted = unsorted.split(" ").sort(function (a, b) {
+    return a - b
 }).join(" ")
-// split() : string => array
-
 
 console.log(sorted)
 
-if (unsorted === sorted) {
-    console.log("yes")
-} else {
-    console.log("No")
-}
