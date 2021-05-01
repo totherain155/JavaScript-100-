1a3c24e595a12ef1ad5f3f3fb749f3cefb7a7eeb
totherain155 <totherain155@gmail.com>
2021-05-01 18:54:47 +0900

    1.May

--- practice37.js
+++ practice37.js
@@ -1,19 +1,3 @@
 const array = ["원범", "원범", "혜원", "혜원", "혜원", "혜원", "유진", "유진"];
 let result = {};
 let winner = "";
-
-for (let index in array) {
-  let val = array[index];
-  result[val] = result[val] === undefined ? 1 : (result[val] = result[val] + 1);
-}
-
-// {원범 : 2, 혜원 : 1}
-
-//console.log(result)
-
-winner = Object.keys(result).reduce(function (a, b) {
-  console.log(a, b);
-  return result[a] > result[b] ? a : b;
-});
-
-console.log(`승자는 ${winner}`);
