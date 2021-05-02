f493474f7c7357c63adcffaa4bf79e8cdb4e757a
totherain155 <totherain155@gmail.com>
2021-05-02 11:47:43 +0900

    2.May

--- practice37.js
+++ practice37.js
@@ -11,21 +11,21 @@ const array = [
   "유진",
   "유진",
 ];
+
 let result = {};
 let winner = "";
 
-for (let index in array) {
+for (index in array) {
   let val = array[index];
   result[val] = result[val] === undefined ? 1 : (result[val] = result[val] + 1);
 }
+
 console.log(result);
 
 winner = Object.keys(result).reduce((a, b) => {
   return result[a] > result[b] ? a : b;
 });
-// Object.keys() 객채의 key 값만 배열로 반환한다.
-// reduce(a, b) 함수를 실행한 값을 a와 b에 계속 중첩을 한다.
 
 console.log(winner);
 
-console.log(`${winner}, ${result[winner]}표를 획득하다.`);
+console.log(`${winner}는(은) ${result[winner]}표를 획득하였습니다. `);
