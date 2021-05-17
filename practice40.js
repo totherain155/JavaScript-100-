1f8f1a1e330666ffcc0015e130a6f9460523c096
totherain155 <totherain155@gmail.com>
2021-05-17 23:16:43 +0900

    17.May

--- practice40.js
+++ practice40.js
@@ -1,11 +1,13 @@
-const weightLimit = "100";
-const memberCount = "5";
+const weightLimit = prompt("무게제한을 입력해주세요");
+const memberCount = prompt("몇명이 탈 계획인가요?");
 let count = 0;
 let totalWeight = 0;
 
 for (let i = 0; i < memberCount; i++) {
-  totalWeight = totalWeight + parseInt("50", 10);
+  totalWeight = totalWeight + parseInt(prompt("몸무게를 입력해주세요"), 10);
   if (totalWeight <= weightLimit) {
     count++;
   }
 }
+
+console.log(count);
