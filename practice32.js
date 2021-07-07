a54302075189636074ed9b3b9980f4fbe4b04a2f
totherain155 <totherain155@gmail.com>
2021-07-07 22:10:34 +0900

    review practice32

--- practice32.js
+++ practice32.js
@@ -1,2 +1,5 @@
 const words = "안녕하세요. 저는 제주대학교 컴퓨터공학전공 혜림입니다.";
-console.log(words.split(" ").length); // 빈문자로 배열을 만들어 반환
+//console.log(words.split(" ")); // 공백을 이용하여 배열을 만들어 반환한다.
+
+//그리고 그 배열의 갯수는
+console.log(words.split(" ").length);
