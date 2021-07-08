598f1c7dfd4762cf00a603df2b4bb9250701c984
totherain155 <totherain155@gmail.com>
2021-07-08 21:58:29 +0900

    review practice33

--- practice33.js
+++ practice33.js
@@ -1,14 +1,22 @@
 const nums = "1 2 3 4 5";
-const arr = prompt().split(" ").reverse().join(" ")
 
-console.log(arr)
-//let reverseVal = "";
+//문자열은 거꾸로 표기할 수 없기 때문에 일단 배열로 바꾸어준다.
+//const arr = nums.split(" ");
 
+//그 후 reverse method로 거꾸로 표기한다.
+//const arr = nums.split(" ").reverse();
+
+// 다시 문자열로 반환한다.
+const arr = nums.split(" ").reverse().join(" ");
+
+console.log(arr);
+
+//let reverseNum = "";
 
 /*
 for (let i = 0; i < arr.length; i++) {
-    reverseVal = reverseVal + (arr[i] + " ");
+    reverseNum = reverseNum + (arr[i] + " ");
 }
 
-console.log(reverseVal)
+console.log(reverseNum)
 */
