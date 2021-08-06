df285990c5bb0d25391a864486d0ad0b562ed399
totherain155 <totherain155@gmail.com>
2021-08-06 16:56:20 +0900

    recap 36

--- practice36.js
+++ practice36.js
@@ -1,8 +1,9 @@
 const num = prompt();
-let result = 0;
 
-for (let i = 1; i <= 9; i++) {
-  result = result + num * i + " ";
+let result = "  ";
+
+for (let i = 0; i < 9; i++) {
+  result = result + num * (i + 1) + " ";
 }
 
 console.log(result);
