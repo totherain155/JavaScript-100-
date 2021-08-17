86be69403dd87c017a5520e4a073deb832cc4e9e
totherain155 <totherain155@gmail.com>
2021-08-17 23:41:15 +0900

    recap practice61

--- practice61.js
+++ practice61.js
@@ -1,21 +1,25 @@
 const user_s = "aaabbbbcdddd";
-let result_s = "";
+
+// 출력할 문자열
+let result_s = " ";
+
+// 저장할 문자열
 let store_s = user_s[0];
+
 let count = 0;
 
-//aaabbbbcdddd
 //result_s = a3b4c1d4
 
 for (let i of user_s) {
   console.log(i);
   if (i == store_s) {
-    count += 1;
+    count++;
   } else {
     result_s += store_s + String(count);
-    store_s = i;
+    store_s = i; // 저장되어 있는 문자열을 현재의 문자열로 바꿔준다.
     count = 1;
   }
 }
+//
 result_s += store_s + String(count);
-
 console.log(result_s);
