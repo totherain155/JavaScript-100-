6cdf140b80938cb2e75eddf3994b75184aceba1c
totherain155 <totherain155@gmail.com>
2021-09-05 00:23:45 +0900

    recap practice69

--- practice69.js
+++ practice69.js
@@ -21,15 +21,15 @@ for (let n of prime) {
   }
 }
 
+console.log(prime);
+
 let 차 = 골드바흐파티션.map((e) => e[1] - e[0]);
 
 console.log(골드바흐파티션);
 console.log(차);
-console.log(Math.min.apply(null, 차));
-console.log(Math.max.apply(null, 차));
 
-let 큰값 = 골드바흐파티션[차.indexOf(Math.max.apply(null, 차))];
-let 작은값 = 골드바흐파티션[차.indexOf(Math.min.apply(null, 차))];
+// 배열에서 가장 작은값이나 큰값을 구하기위해선 apply함수가 필요하다.
+const 큰값 = 골드바흐파티션[차.indexOf(Math.max.apply(null, 차))];
+const 작은값 = 골드바흐파티션[차.indexOf(Math.min.apply(null, 차))];
 
-console.log(큰값);
-console.log(작은값);
+console.log(큰값, 작은값);
