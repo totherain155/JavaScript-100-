4c9d369255cf53427209602f79741b86f1fd7df8
totherain155 <totherain155@gmail.com>
2021-09-18 11:12:26 +0900

    practice72

---
+++ practice72.js
@@ -0,0 +1,27 @@
+const graph = {
+  A: ["E", "C", "B"],
+  B: ["A"],
+  C: ["A"],
+  D: ["E", "F"],
+  E: ["D", "A"],
+  F: ["D"],
+};
+
+function bfs(graph, start) {
+  let visited = [];
+  let queue = [start];
+
+  while (queue.length !== 0) {
+    let n = queue.shift();
+    if (!visited.includes(n)) {
+      visited.push(n);
+      let sub = graph[n].filter((x) => !visited.includes(x));
+      for (let i of sub) {
+        queue.push(i);
+      }
+    }
+  }
+  return visited;
+}
+
+console.log(bfs(graph, "E"));
